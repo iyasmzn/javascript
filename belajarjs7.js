46fb6b4a4c01fad8c91cef99785235b06e030a3b
iyasmzn <iyasmzn07@gmail.com>
2019-12-02 12:55:07 +0700

    first add

---
+++ belajarjs7.js
@@ -0,0 +1,12 @@
+var pembanding1 = 5;
+var pembanding2 = 5;
+var tag_hasil = document.getElementById('hasil');
+
+if (pembanding1 == pembanding2){
+	tag_hasil.innerHTML = "true";
+	}
+else {
+	tag_hasil.innerHTML = "false";
+	}
+	
+
